88589fb1f86e78a45cd0f11930c338bf980ca36f
Jacqui Lough <lough.jacqui@gmail.com>
2026-07-26 14:12:57 +1000

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div className="flex flex-col flex-1 items-center font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col py-32 px-16 bg-white">
+      <main className="flex flex-1 w-full max-w-3xl flex-col px-16 bg-white">
         <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black mb-10">
           About Me
         </h1>
